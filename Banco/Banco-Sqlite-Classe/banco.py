3bb4c38f86529bb126f3f75ad19e701a76ebf610
FeHunter <hunterfe1111@gmail.com>
2024-11-18 08:36:45 -0300

    conexão com banco de dados sqlite3

--- Banco/Banco-Sqlite-Classe/banco.py
+++ Banco/Banco-Sqlite-Classe/banco.py
@@ -1,17 +1,14 @@
 from crud import *
 from menu import *
-from arquivo import *
 
-contas = ler_conta()
-opcao = entrar_opcao()
-while (opcao != 0):
-    match (opcao):
-        case 1: incluir_conta(contas)
-        case 2: alterar_conta(contas)
-        case 3: excluir_conta(contas)
-        case 4: consultar_conta(contas)
-        case 5: consultar_contas(contas)
-        case _: print("Erro: opção inválida")
-    opcao = entrar_opcao()
-
-gravar_contas(contas)
+# contas = []
+# opcao = entrar_opcao()
+# while (opcao != 0):
+#     match (opcao):
+#         case 1: incluir_conta(contas)
+#         case 2: alterar_conta(contas)
+#         case 3: excluir_conta(contas)
+#         case 4: consultar_conta(contas)
+#         case 5: consultar_contas(contas)
+#         case _: print("Erro: opção inválida")
+#     opcao = entrar_opcao()
